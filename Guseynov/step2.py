baa3af8b1cb8e3318eba4c861da61b1153806788
GuseynovGM <71015870+GuseynovGM@users.noreply.github.com>
2022-03-25 15:16:49 +0300

    update and rename step1.py to step2.py
    реализована функция булеана

--- Guseynov/step2.py
+++ Guseynov/step2.py
@@ -1,14 +1,20 @@
+import itertools
+from itertools import combinations
 class Set:
-    def __init__(self, lst_of_items):
+    def __init__(self, lst_of_items=None):
         self._list = []
-        for item in lst_of_items:
-            self.add(item)
+        if lst_of_items is not None:
+            for item in lst_of_items:
+                self.add(item)
 
     def __str__(self):
         return str(" ".join(map(str, self._list)))
 
     def __iter__(self):
         return iter(self._list.copy())
+
+    def __len__(self):
+        return len(self._list)
 
     def add(self, item):
         if item not in self._list:
@@ -29,6 +35,15 @@ class Set:
     def addiction(set1, set2):
         return Set.difference(set2, set1)
 
+    def boolean(self):
+        boolean = Set()
+        for i in range(len(self) + 1):
+            temp = list(itertools.combinations(self._list, i))
+            for j in temp:
+                boolean.add(j)
+        return boolean
+
+
 
 if __name__ == '__main__':
     print("	☆*:.｡.o(≧▽≦)o.｡.:*☆")
@@ -44,5 +59,7 @@ if __name__ == '__main__':
         print(Set.difference(set1, set2))
     elif operation == "a":
         print(Set.addiction(set1, set2))
+    elif operation == "b": # boolean
+        print(set1.boolean())
     else:
         print(">.<")
