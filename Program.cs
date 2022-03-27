d2cfe009fcfb71c17f453c15d5d9fdaad0d03b73
nguyentienlong1106 <71026080+nguyentienlong1106@users.noreply.github.com>
2022-03-27 17:09:03 +0300

    Этап 3

--- Program.cs
+++ Program.cs
@@ -1,51 +1,106 @@
 ﻿using System;
 using static System.Console;
 
-namespace Булеан
+
+namespace Binaryrelation
 {
     internal class Program
     {
-        public static int[] a = new int[100];
-        public static int[] Enter(int x, int[] b)
+        private static bool IsIn(int[][] b, int x, int y)
         {
-            for (int i = 1; i <= x; i++)
+            for (int i = 0; i < b.Length; i++)
             {
-                Write("Enter element b[{0}] = ", i);
-                b[i] = int.Parse(ReadLine());
+                if (b[i][0] == x && b[i][1] == y)
+                {
+                    return true;
+                }
             }
-            return b;
+            return false;
         }
-        public static void Out(int k, int[] b)
+        public static string transitivily(int[][] b, int y)
         {
-            for (int i = 1; i <= k; i++)
+            int[] s = new int[y];
+            for (int i = 0; i < y; i++)
             {
-                Write(b[a[i]]);
+                s[i] = b[i][1];
             }
-            WriteLine();
+            int[] c = new int[y];
+
+            for (int i = 0; i < y; i++)
+            {
+                c[i] = b[i][0];
+            }
+            for (int i = 0; i < y; i++)
+            {
+                if (!IsIn(b, s[i], s[i]) || !IsIn(b, c[i], c[i]))
+                {
+
+                    return "not transitibily";
+                }
+            }
+            return "transitivily";
+
         }
-        public static void Boolean(int i, int x, int k, int[] b)
+        public static string symmetry(int[][] b, int y)
         {
-            for (int j = a[i - 1] + 1; j <= x; j++)
+            int[] s = new int[y];
+            for (int i = 0; i < y; i++)
             {
-                a[i] = j;
-                if (i == k) Out(k, b);
-                else Boolean(i + 1, x, k, b);
+                s[i] = b[i][1];
             }
-            
+            int[] c = new int[y];
+            for (int i = 0; i < y; i++)
+            {
+                c[i] = b[i][0];
+            }
+            for (int i = 0; i < y; i++)
+            {
+                if (!IsIn(b, s[i], c[i]))
+                {
+                    return "not symmetry";
+                }
+            }
+            return "symmetry";
         }
+        public static string reflexivity(int[][] b, int y)
+        {
+            int[] c = new int[y];
+            for (int i = 0; i < y; i++)
+            {
+                c[i] = b[i][0];
+            }
+            for (int i = 0; i < y; i++)
+            {
+                if (!IsIn(b, c[i], c[i]))
+                {
+                    return "not reflexivity";
+                }
+            }
+            return "reflexivity";
+        }
+
         static void Main(string[] args)
         {
-            Write("Enter the size of asemble: ");
-            int x = int.Parse(ReadLine());
-            int[] b = new int[x+1];
-            b = Enter(x, b);
-            a[0] = 0;
-
-            WriteLine("[]");
-
-            for(int k=1; k<=x; k++)
+            string[] x_y = ReadLine().Split(new[] { ' ' }); // nhập dãy số vào mảng, nhập string được chia cắt 
+            int x = int.Parse(x_y[0]);                      // вершин
+            int y = int.Parse(x_y[1]);
+            if (y <= Math.Pow(2, x))
             {
-                Boolean(1, x, k, b);
+                int[][] b = new int[y][];             // b[][] = [[1,2], [3,4], ....] : ребер
+                for (int i = 0; i < y; i++)
+                {
+                    x_y = null;
+                    x_y = ReadLine().Split(new[] { ' ' });
+                    b[i] = new int[] { int.Parse(x_y[0]), int.Parse(x_y[1]) };
+                }
+                //WriteLine(b[2][0]);
+                WriteLine(transitivily(b, y));
+                WriteLine(symmetry(b, y));
+                WriteLine(reflexivity(b, y));
+            }
+            else
+            {
+                WriteLine("ERROR!!");
             }
         }
     }
